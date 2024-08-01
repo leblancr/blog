785b0c4114162dfccdfceee1f257daece558aea7
leblancr <rich.leblanc1@proton.me>
2024-08-01 12:22:26 -0400

    Initial checkin

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # maps the root path of our application to a controller and action.
+  root 'articles#index' # in views?
+
+  # map /articles url to app/controllers/articles_controller.rb index function
+  get '/articles', to: 'articles#index'
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
