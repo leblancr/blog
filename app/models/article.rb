7bc1347e43e9850f289e90e5ade6fd000154f96e
leblancr <rich.leblanc1@proton.me>
2024-08-01 20:52:16 -0400

    Finished 8 added comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,8 @@
 # Each article must have a title and body. The body must be
 # at least 10 characters long.
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   root 'articles#index' # in views?
 
   # map /articles url to app/controllers/articles_controller.rb index function
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
